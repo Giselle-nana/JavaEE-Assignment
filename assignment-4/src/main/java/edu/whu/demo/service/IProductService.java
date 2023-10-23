575c901b5cb44b07f4a56b37c05325c5cce9570f
Giselle-nana <126684316+Giselle-nana@users.noreply.github.com>
2023-10-23 15:45:25 +0800

    Delete assignment-4 directory

--- assignment-4/src/main/java/edu/whu/demo/service/IProductService.java
+++
@@ -1,7 +0,0 @@
-package edu.whu.demo.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import edu.whu.demo.domain.Product;
-
-public interface IProductService extends IService<Product> {
-}
--- assignment-4/src/main/java/edu/whu/demo/service/ISupplierService.java
+++
@@ -1,7 +0,0 @@
-package edu.whu.demo.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import edu.whu.demo.domain.Supplier;
-
-public interface ISupplierService extends IService<Supplier> {
-}
